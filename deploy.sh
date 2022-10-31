f03f9fd3b27448d68e21bd4b44dbc61d66baeb75
Tim Boldt <tim.boldt@gmail.com>
2022-10-30 20:33:36 -0700

    Updated Pi compilation

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-readonly TARGET_HOST=pizw2
+readonly TARGET_HOST=pidev
 readonly TARGET_PATH=/home/pi/iot-central
-readonly TARGET_ARCH=armv7-unknown-linux-gnueabihf
+readonly TARGET_ARCH=aarch64-unknown-linux-gnu
 readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/iot-central
 
 cargo build --release --target=${TARGET_ARCH}
