11ffca294da424bcb4e2c044b7c0975bd9cf1133
Tim Boldt <tim.boldt@gmail.com>
2022-11-05 14:01:57 -0700

    Fixed docker build/deploy to Pi mechanism

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ readonly TARGET_HOST=pizw2
 readonly TARGET_PATH=/home/pi/bin/iot-central
 readonly SOURCE_PATH=./target/release/iot-central
 
-docker run --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/myapp -w /usr/src/myapp rust:bullseye cargo build --release
+docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/myapp -w /usr/src/myapp rust:bullseye cargo build --release --no-default-features --features rpi
 rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
 ssh -t ${TARGET_HOST} sudo systemctl restart iot-central.service
