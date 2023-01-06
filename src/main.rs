18e138514436dc6dcab8c0a463167c18e1699066
Tim Boldt <tim.boldt@gmail.com>
2023-01-05 22:12:02 -0800

    Partial fix to smol hyper issues

--- src/main.rs
+++ src/main.rs
@@ -29,66 +29,68 @@ use std::env;
 use std::sync::{Arc, Condvar, Mutex};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
-
-    let shutdown = Arc::new((Mutex::new(false), Condvar::new()));
-    let (tx, rx) = async_channel::bounded(32);
-
-    // Start the Adafruit IO transmission agent.
-    let aio_params = adafruit::CallParams {
-        base_url: "https://io.adafruit.com/api/v2".to_owned(),
-        io_user: env::var("IO_USERNAME").expect("Adafruit IO_USERNAME is not defined."),
-        io_key: env::var("IO_KEY").expect("Adafruit IO_KEY is not defined."),
-    };
-    let aio_thread = smol::spawn(async move { adafruit::aio_sender(aio_params, rx).await });
-
-    // Start the sensor thread.
-    let sensor_params = sensor::CallParams {
-        shutdown: shutdown.clone(),
-        tx: tx.clone(),
-    };
-    let sensor_thread = smol::spawn(async move { sensor::sensor_updater(sensor_params).await });
-
-    // Start the finance thread.
-    let finance_params = finance::CallParams {
-        shutdown: shutdown.clone(),
-        tx: tx.clone(),
-        base_url: "https://finnhub.io/api/v1/quote".to_owned(),
-        api_key: env::var("FINHUB_API_KEY").expect("FINHUB_API_KEY is not defined."),
-        symbols: vec![
-            "DIA".into(),
-            "COINBASE:BTC-USD".into(),
-            "BITFINEX:USTUSD".into(),
-            "KRAKEN:USDTZUSD".into(),
-            "QQQ".into(),
-        ],
-    };
-    let finance_thread = smol::spawn(async move { finance::finance_updater(finance_params).await });
-
-    // Start the weather thread.
-    let weather_params = weather::CallParams {
-        shutdown: shutdown.clone(),
-        tx: tx.clone(),
-        base_url: "https://api.openweathermap.org/data/2.5/onecall".to_owned(),
-        api_key: env::var("OPEN_WEATHER_KEY").expect("OPEN_WEATHER_KEY is not defined."),
-        lat: env::var("OPEN_WEATHER_LAT").expect("OPEN_WEATHER_LAT is not defined."),
-        lon: env::var("OPEN_WEATHER_LON").expect("OPEN_WEATHER_LON is not defined."),
-        units: "metric".to_owned(),
-    };
-    let weather_thread = smol::spawn(async move { weather::weather_updater(weather_params).await });
-
-    ctrlc::set_handler(move || {
-        info!("Shutdown initiated...");
-
-        // Signal all the producer threads.
-        let (lock, cvar) = &*shutdown;
-        let mut sig = lock.lock().unwrap();
-        *sig = true;
-        cvar.notify_all();
-    })
-    .unwrap();
-
     smol::block_on(async {
+        env_logger::init();
+
+        let shutdown = Arc::new((Mutex::new(false), Condvar::new()));
+        let (tx, rx) = async_channel::bounded(32);
+
+        // Start the Adafruit IO transmission agent.
+        let aio_params = adafruit::CallParams {
+            base_url: "https://io.adafruit.com/api/v2".to_owned(),
+            io_user: env::var("IO_USERNAME").expect("Adafruit IO_USERNAME is not defined."),
+            io_key: env::var("IO_KEY").expect("Adafruit IO_KEY is not defined."),
+        };
+        let aio_thread = smol::spawn(async move { adafruit::aio_sender(aio_params, rx).await });
+
+        // Start the sensor thread.
+        let sensor_params = sensor::CallParams {
+            shutdown: shutdown.clone(),
+            tx: tx.clone(),
+        };
+        let sensor_thread = smol::spawn(async move { sensor::sensor_updater(sensor_params).await });
+
+        // Start the finance thread.
+        let finance_params = finance::CallParams {
+            shutdown: shutdown.clone(),
+            tx: tx.clone(),
+            base_url: "https://finnhub.io/api/v1/quote".to_owned(),
+            api_key: env::var("FINHUB_API_KEY").expect("FINHUB_API_KEY is not defined."),
+            symbols: vec![
+                "DIA".into(),
+                "COINBASE:BTC-USD".into(),
+                "BITFINEX:USTUSD".into(),
+                "KRAKEN:USDTZUSD".into(),
+                "QQQ".into(),
+            ],
+        };
+        let finance_thread =
+            smol::spawn(async move { finance::finance_updater(finance_params).await });
+
+        // Start the weather thread.
+        let weather_params = weather::CallParams {
+            shutdown: shutdown.clone(),
+            tx: tx.clone(),
+            base_url: "https://api.openweathermap.org/data/2.5/onecall".to_owned(),
+            api_key: env::var("OPEN_WEATHER_KEY").expect("OPEN_WEATHER_KEY is not defined."),
+            lat: env::var("OPEN_WEATHER_LAT").expect("OPEN_WEATHER_LAT is not defined."),
+            lon: env::var("OPEN_WEATHER_LON").expect("OPEN_WEATHER_LON is not defined."),
+            units: "metric".to_owned(),
+        };
+        let weather_thread =
+            smol::spawn(async move { weather::weather_updater(weather_params).await });
+
+        ctrlc::set_handler(move || {
+            info!("Shutdown initiated...");
+
+            // Signal all the producer threads.
+            let (lock, cvar) = &*shutdown;
+            let mut sig = lock.lock().unwrap();
+            *sig = true;
+            cvar.notify_all();
+        })
+        .unwrap();
+
         info!("Waiting for Sensor thread...");
         sensor_thread.await;
 
