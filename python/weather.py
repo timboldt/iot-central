cf29fbe12435032c34c4472e98689b929ecb3063
Tim Boldt <tim.boldt@gmail.com>
2023-02-06 18:58:23 -0800

    Made weather fetcher more reliable

--- python/weather.py
+++ python/weather.py
@@ -1,30 +1,43 @@
 import asyncio
-
-from pyowm import OWM
-
+import pyowm
+import pyowm.commons.exceptions as owmexceptions
 
 class Fetcher:
     def __init__(self, sender: object, open_weather_key: str, lat: float, lon: float) -> None:
-        self._UPDATE_PERIOD = 10.0 * 60.0
+        self.UPDATE_PERIOD = 10.0 * 60.0
         self._sender = sender
-        self._api = OWM(open_weather_key)
+        self._api = pyowm.OWM(open_weather_key)
+        self._mgr = self._api.weather_manager()
         self.lat = lat
         self.lon = lon
 
     async def run(self):
         print("Weather Fetcher started")
-        mgr = self._api.weather_manager()
-        while True:
-            print("Weather Fetcher requesting data")
-            res = mgr.one_call(lat=self.lat, lon=self.lon)
-            print("Weather Fetcher got data")
-            print("Weather:", res.current.temperature("celsius"),
-                  res.current.humidity, res.current.barometric_pressure("hPa"))
-            try:
-                await self._sender.send("weather.temp", res.current.temperature("celsius")["temp"])
-                await self._sender.send("weather.humidity", res.current.humidity)
-                await self._sender.send("weather.pressure", res.current.barometric_pressure("hPa")["press"])
-                await asyncio.sleep(self._UPDATE_PERIOD)
-            except asyncio.exceptions.CancelledError:
-                print("Weather Fetcher stopped")
-                return
+        try:
+            while True:
+                res = self._fetch()
+                if res is not None:
+                    await self._send()
+                await asyncio.sleep(self.UPDATE_PERIOD)
+        except asyncio.exceptions.CancelledError:
+            print("Weather Fetcher stopped")
+            return
+       
+    def _fetch(self):
+        print("Weather Fetcher requesting data...")
+        try:
+            res = self._mgr.one_call(lat=self.lat, lon=self.lon)
+        except owmexceptions.PyOWMError as err:
+            print("Error fetching weather data: {}".format(err))
+            return None
+        print("Weather is:", res.current.temperature("celsius"),
+            res.current.humidity, res.current.barometric_pressure("hPa"))
+
+    async def _send(self, res: object):
+        try:
+            await self._sender.send("weather.temp", res.current.temperature("celsius")["temp"])
+            await self._sender.send("weather.humidity", res.current.humidity)
+            await self._sender.send("weather.pressure", res.current.barometric_pressure("hPa")["press"])
+        except asyncio.exceptions.CancelledError:
+            print("Weather Fetcher stopped")
+            return
