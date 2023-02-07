52a8afb1badad8e5a76cb07129a6cee11245b82d
Tim Boldt <tim.boldt@gmail.com>
2023-02-06 19:25:11 -0800

    Cleanup

--- python/adafruit.py
+++ python/adafruit.py
@@ -1,6 +1,5 @@
-import asyncio
-
 import Adafruit_IO
+import asyncio
 
 
 class Message:
@@ -15,6 +14,7 @@ class Sender:
         self._queue = asyncio.Queue(QUEUE_SIZE)
         self._io_user = io_user
         self._io_key = io_key
+        self._aio = Adafruit_IO.Client(self._io_user, self._io_key)
 
     async def send(self, feed: str, value: str) -> None:
         await self._queue.put(Message(feed, value))
@@ -23,19 +23,21 @@ class Sender:
         await self._queue.join()
 
     async def run(self) -> None:
-        aio = Adafruit_IO.Client(self._io_user, self._io_key)
-        print("Adafruit IO Sender started")
-        while True:
-            try:
+        try:
+            print("Adafruit IO Sender started")
+            while True:
                 msg = await self._queue.get()
-            except asyncio.exceptions.CancelledError:
-                print("Adafruit IO Sender stopped")
-                return
-            try:
-                aio.send_data(msg.feed, msg.value)
-                print("Adafruit IO sender sent: {}/{}".format(msg.feed, msg.value))
-            except Adafruit_IO.errors.RequestError as err:
-                print("Adafruit IO Sender request error: {}".format(err))
-            except Adafruit_IO.errors.AdafruitIOError as err:
-                print("Adafruit IO Sender general error: {}".format(err))
-            self._queue.task_done()
+                self._send(msg)
+                self._queue.task_done()
+        except asyncio.exceptions.CancelledError:
+            print("Adafruit IO Sender stopped")
+            return
+
+    def _send(self, msg: Message) -> None:
+        try:
+            self._aio.send_data(msg.feed, msg.value)
+            print("Adafruit IO sender sent: {}/{}".format(msg.feed, msg.value))
+        except Adafruit_IO.errors.RequestError as err:
+            print("Adafruit IO Sender request error: {}".format(err))
+        except Adafruit_IO.errors.AdafruitIOError as err:
+            print("Adafruit IO Sender general error: {}".format(err))
