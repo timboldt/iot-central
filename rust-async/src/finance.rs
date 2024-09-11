15b48cb12d91b2555b4bc34cd6b9f209c50037d9
Tim Boldt <tim.boldt@gmail.com>
2024-09-10 21:35:56 -0700

    Add simplistic service

--- rust-async/src/finance.rs
+++ rust-async/src/finance.rs
@@ -1,21 +1,32 @@
 use finnhub_rs::client::Client;
+use tokio::time::{sleep, Duration};
 
 pub struct Finance {
     api_key: String,
+    symbols: Vec<String>,
 }
 
 impl Finance {
-    pub fn new(api_key: &str) -> Self {
+    pub fn new(api_key: &str, symbols: Vec<String>) -> Self {
         Finance {
             api_key: api_key.to_owned(),
+            symbols: symbols,
+        }
+    }
+
+    pub async fn run(&self) {
+        // Lame implementation; needs graceful shutdown, etc.
+        loop {
+            self.process().await;
+            sleep(Duration::from_secs(3600)).await;
         }
     }
 
     pub async fn process(&self) {
-        let client = Client::new(self.api_key.clone());
-        // Get a list of supported stocks given the exchange.
-        let res = client.stock_symbol("US".to_string()).await.unwrap();
-        // Print out the results.
-        println!("{:#?}", res);
+        for symbol in &self.symbols {
+            let client = Client::new(self.api_key.clone());
+            let res = client.quote(symbol.to_string()).await.unwrap();
+            println!("{} {:#?}", symbol, res);
+        }
     }
 }
--- rust-async/src/main.rs
+++ rust-async/src/main.rs
@@ -9,8 +9,11 @@ async fn main() -> io::Result<()> {
     println!("Starting...");
 
     let api_key = env::var("FINHUB_API_KEY").expect("FINHUB_API_KEY env variable is missing");
-    let f = finance::Finance::new(&api_key);
-    f.process().await;
+    let f = finance::Finance::new(
+        &api_key,
+        vec!["AAPL".to_owned(), "GOOG".to_owned(), "MSFT".to_owned()],
+    );
+    f.run().await;
     println!("Running (^C to exit)");
 
     match signal::ctrl_c().await {
